8ed91a4b84646b594af07fca8734473b50f9f7e7
samuel <wachtel.s@icloud.com>
2025-11-07 09:36:16 +0100

    chore: create folder structure

---
+++ src/InvoiceManager.Presentation/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace InvoiceManager.Presentation.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ src/InvoiceManager.Presentation/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace InvoiceManager.Presentation.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
